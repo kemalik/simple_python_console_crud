9a97bcb7866b3f11fd6d1d4475baf593455f8a34
kema <kekenov.malik@gmail.com>
2016-11-24 23:18:00 +0600

    add create record function

--- main.py
+++ main.py
@@ -20,8 +20,36 @@ def print_to_console(records=None):
     else:
         print u'Нет записей'
 
+
+def create_record(records=None):
+    make = raw_input(u'Введите марку машины')
+    model = raw_input(u'Введите модель машины')
+    year = raw_input(u'Введите год выпуска машины')
+    for record in records:
+        if set([make, model, year]).issubset(record.values()):
+            print u'Машина {make} {model} {year} года уже есть в базе'
+            return
+    records.append({
+        'id': 1,
+        'make': make,
+        'model': model,
+        'year': year
+    })
+    print u'Запись успешно добавлена'
+
+
+
+
 def main():
-    print_to_console(cars)
+    action = 1
+    while action != 0:
+        action = raw_input(u'0 - выйти\n1 - показать записи\n2 - добавить запись\n\nВыберите действие: ')
+        if action == '1':
+            print_to_console(cars)
+        elif action == '2':
+            create_record(cars)
+        else:
+            break
 
 
 if __name__ == '__main__':
