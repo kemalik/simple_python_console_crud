9124d16822ceba8542a640f93b4571fdd03f7533
kema <kekenov.malik@gmail.com>
2016-11-25 00:50:51 +0600

    add update function

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python
 # coding:utf-8
 
+translits = {
+    'id': 'id',
+    'make': u'Марка',
+    'model': u'Модель',
+    'year': u'Год выпуска'
+}
+
 cars = [
     {'id': '1', 'make': 'BMW', 'model': 'X5', 'year': '2010'},
     {'id': '2', 'make': 'Lamborghini', 'model': 'Aventador', 'year': '2011'},
     {'id': '3', 'make': 'Bugatti', 'model': 'Veyrona', 'year': '2007'},
     {'id': '4', 'make': 'Toyota', 'model': 'Camry', 'year': '2015'},
 ]
+
 
 def print_to_console(records=None):
     if records:
@@ -52,16 +60,43 @@ def delete_record(records=None):
     else:
         print u'Нет записей'
 
+
+def update_record(records=None):
+    if records:
+        print_to_console(records)
+        record_id = raw_input(u'Введите id записи')
+        for record in records:
+            if record['id'] == record_id:
+                for key, value in record.items():
+                    if key == 'id':
+                        continue
+                    new_record = raw_input(
+                        u'Текущее значение {current_name}: {current_record}, введите новое значение либо оставьте пустым: '.format(
+                            current_name=translits.get(key), current_record=value
+                        )
+                    )
+                    if new_record:
+                        record[key] = new_record
+                        print u'Запись успешно обновлена'
+                break
+        else:
+            print u'Запись с таким id не найдена'
+    else:
+        print u'Нет записей'
+
+
 def main():
     action = 1
     while action != 0:
-        action = raw_input(u'0 - выйти\n1 - показать записи\n2 - добавить запись\n3 - Удалить запись\n\nВыберите действие: ')
+        action = raw_input(u'0 - выйти\n1 - показать записи\n2 - добавить запись\n3 - Удалить запись\n4 - Изменить запись\n\nВыберите действие: ')
         if action == '1':
             print_to_console(cars)
         elif action == '2':
             create_record(cars)
         elif action == '3':
             delete_record(cars)
+        elif action == '4':
+            update_record(cars)
         else:
             print u'Нет действия на {action}'.format(action=action)
 
