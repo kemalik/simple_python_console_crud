2d2ad7f693a89c2403cba253cbb6341b2c52ec97
kema <kekenov.malik@gmail.com>
2016-11-24 22:39:01 +0600

    add cars dictionary and print them

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python
 # coding:utf-8
 
+cars = [
+    {'id': 1, 'make': 'BMW', 'model': 'X5', 'year': 2010},
+    {'id': 2, 'make': 'Lamborghini', 'model': 'Aventador', 'year': 2011},
+    {'id': 3, 'make': 'Bugatti', 'model': 'Veyrona', 'year': 2007},
+    {'id': 4, 'make': 'Toyota', 'model': 'Camry', 'year': 2015},
+]
+
 def main():
-    print "Hello, World!"
+    print "===================================="
+    print u'| id | Марка | Модель | Год выпкска|'
+    for car in cars:
+        print u'| {id} | {make} | {model} | {year} |'.format(
+            id=car['id'], make=car['make'], model=car['model'], year=car['year']
+        )
+    print "===================================="
 
 
 if __name__ == '__main__':
