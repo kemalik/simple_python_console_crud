9aaa0ab690edeb1f3ca2dd67b6b6a7db4cf59c10
kema <kekenov.malik@gmail.com>
2016-11-25 02:59:36 +0600

    data in another module

---
+++ data.py
@@ -0,0 +1,31 @@
+# coding:utf-8
+
+MAIN_MENU = u'''
+0 - выйти
+1 - показать записи
+2 - добавить запись
+3 - Удалить запись
+4 - Изменить запись
+
+Выберите действие: '''
+
+translits = {
+    'id': u'id',
+    'make': u'Марка',
+    'model': u'Модель',
+    'year': u'Год выпуска'
+}
+
+cars = [
+    {'id': '1', 'make': 'BMW', 'model': 'X5', 'year': '2010'},
+    {'id': '2', 'make': 'Lamborghini', 'model': 'Aventador', 'year': '2011'},
+    {'id': '3', 'make': 'Bugatti', 'model': 'Veyrona', 'year': '2007'},
+    {'id': '4', 'make': 'Toyota', 'model': 'Camry', 'year': '2015'},
+]
+
+spaces = {
+    'id': 2,
+    'make': 2,
+    'model': 2,
+    'year': 2
+}
